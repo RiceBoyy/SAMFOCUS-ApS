6a625379e3aa74e16b6627e0730bd88036af60c9
RiceBoyy <owen.j.de.guzman@gmail.com>
2024-09-13 22:38:46 +0200

    test

--- samfocus-aps/next.config.mjs
+++ samfocus-aps/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: '/SAMFOCUS-ApS/samfocus-aps',
+    assetPrefix: '/SAMFOCUS-ApS/samfocus-aps',
+};
 
 export default nextConfig;
